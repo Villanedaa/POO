a69ab56e2bcdfe23473143b459c36a53423d4209
Sebastian <sebastianvillaneda202@gmail.com>
2025-03-15 14:25:32 -0500

    [sebastian_villaneda]: Se agrego un nuevo proyecto

---
+++ Procesador_de_pagos/src/procesador_de_pagos/Procesador_de_pagos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package procesador_de_pagos;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class Procesador_de_pagos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
