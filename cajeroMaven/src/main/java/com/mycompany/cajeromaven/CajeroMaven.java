004b2492450e2393ff9dde48757a1fa19fd24f22
Sebastian <sebastianvillaneda202@gmail.com>
2025-05-19 11:19:39 -0500

    Se agrego un nuevo proyecto

---
+++ cajeroMaven/src/main/java/com/mycompany/cajeromaven/CajeroMaven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cajeromaven;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class CajeroMaven {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
